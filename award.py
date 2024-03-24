26afdc65f78ac57afadd97840882c5b89c40616e
Audrey Njoku <audreynjoku@yahoo.com>
2024-03-24 18:57:47 +0000

    SUPER HERO award added

--- award.py
+++ award.py
@@ -6,6 +6,7 @@ calculate total time
 display total time
 assign award based on finishing time
 if more than 111 minutes then no reward
+edited to add SUPER HERO AWARD 240324
 """
 
 #requesting times of races as integers to enable later calculating.
@@ -16,7 +17,9 @@ total_time = swimming + cycling + running #calculating total time of race.
 print(f"Your total race time is : {total_time} minutes.") # printing total time of race as requested. 
 
 #conditional statements assigning the correct rewards based on total finishing time. 
-if total_time <= 100 :
+if total_time <= 80:
+    print("You are an absolute SUPER HERO! you win £100,000")
+elif total_time <= 100 :
     print("Congratulations on your Provincial colours award!")
 elif total_time <= 105 :
     print("Congratulations on your Provincial half colours award!")
